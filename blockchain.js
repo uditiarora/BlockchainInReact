a14050308d28fafa8c1242bf52c0c84414b6990f
Uditi <uditiarora@gmail.com>
2020-01-05 16:11:57 +0530

    done with chain validation

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const cryptoHash = require('./crypto-hash');
 class Blockchain {
     constructor() {
         this.chain = [Block.genesis()];
@@ -11,6 +11,35 @@ class Blockchain {
             data
         });
         this.chain.push(newBlock);
+    }
+
+    replaceChain(chain){
+        if(chain.length <= this.chain.length){
+            console.error('the incoming chain must be longer');
+            return;
+        }
+        if(!Blockchain.isValidChain(chain)){
+            console.error('the incoming chain must be valid');
+            return;
+        }
+        console.log('replacing chain with', chain);
+        this.chain = chain;
+    }
+    static isValidChain(chain){
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())){
+            return false;
+        }
+        for(let i=1; i<chain.length;i++){
+            const block = chain[i];
+            const actualLastHash = chain[i-1].hash;
+            const {timestamp, lastHash, hash, data} = block;
+            if(lastHash !== actualLastHash) return false;
+            const validatedHash = cryptoHash(timestamp, lastHash,data);
+            if(hash != validatedHash) return false;
+
+        }
+
+        return true;
     }
 }
 
