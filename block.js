1128f829df651d33ea2d9abbf1db6774ba537d48
Uditi <uditiarora@gmail.com>
2020-01-05 13:42:25 +0530

    done with block class

--- block.js
+++ block.js
@@ -1,15 +1,28 @@
+const cryptoHash = require('./crypto-hash');
 const {GENESIS_DATA} = require('./config');
 class Block {
     constructor({ timestamp, lastHash, hash ,data }){
         this.timestamp = timestamp;
         this.lastHash = lastHash;
-        this. hash = hash;
+        this.hash = hash;
         this.data = data;
     }
 
     static genesis(){
         return new Block(GENESIS_DATA);
     }
+
+    static mineBlock({lastBlock, data}){
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
+        return new this({
+            timestamp,
+            lastHash,
+            data,
+            hash : cryptoHash(timestamp,lastHash,data)
+        });
+    } 
 }
 
 module.exports = Block;
