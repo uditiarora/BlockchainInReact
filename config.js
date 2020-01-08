668621ad4d4e20ec604e9c99867f392a39ad74f2
Uditi <uditiarora@gmail.com>
2020-01-08 20:18:12 +0530

    adding transaction feature

--- config.js
+++ config.js
@@ -2,11 +2,23 @@ const MINE_RATE = 1000; //1s
 const INITIAL_DIFFICULTY = 3;
 
 const GENESIS_DATA = {
-    timestamp : '-----',
+    timestamp : 1,
+    lastHash:'-----',
     hash : 'hash-one',
     difficulty : INITIAL_DIFFICULTY,
     nonce: 0,
     data : []
 };
+const STARTING_BALANCE = 1000;
 
-module.exports = { GENESIS_DATA, MINE_RATE};
+const REWARD_INPUT = { address: '*authorized-reward*' };
+
+const MINING_REWARD = 50;
+
+module.exports = {
+  GENESIS_DATA,
+  MINE_RATE,
+  STARTING_BALANCE,
+  REWARD_INPUT,
+  MINING_REWARD
+};
--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -1,10 +1,9 @@
 const crypto = require('crypto');
 
 const cryptoHash = (...inputs) => {
-    const hash = crypto.createHash('sha256');
-    hash.update(inputs.sort().join(' '));
-    return hash.digest('hex');
+  const hash = crypto.createHash('sha256');
+  hash.update(inputs.map(input => JSON.stringify(input)).sort().join(' '));
+  return hash.digest('hex');
 };
-
 
 module.exports = cryptoHash;
