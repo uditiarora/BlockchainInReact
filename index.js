2461e7ce6ec13c9058fc6f66f998c05fab25833d
Uditi <uditiarora@gmail.com>
2020-01-09 10:47:25 +0530

    basic synced transaction pool

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 const app = express();
 const DEFAULT_PORT = 3000;
 
 const blockchain = new Blockchain();
-const pubsub = new PubSub({blockchain});
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const pubsub = new PubSub({blockchain, transactionPool,wallet});
 
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
@@ -24,14 +28,42 @@ app.post('/api/mine',(req,res) => {
     res.redirect('/api/blocks');
 });
 
-const syncChains = () => {
+app.post('/api/transact',(req,res) => {
+    const { amount, recipient } = req.body;
+    let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey});
+    try{
+        if(transaction){
+            transaction.update({senderWallet: wallet, recipient, amount});
+        }
+        else{
+            transaction = wallet.createTransaction({recipient, amount});
+        }
+    }catch(error){
+        return res.status(404).json({type: 'error', message: error.message});
+    }
+    transactionPool.setTransaction(transaction);
+    pubsub.broadcastTransaction(transaction);
+    res.json({type: 'success', transaction});
+});
+
+app.get('/api/transaction-pool-map',(req,res) => {
+    res.json(transactionPool.transactionMap);
+});
+const syncWithRootState = () => {
     request({url: `${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body) => {
-        if(!error && response.statusCode==200){
+        if(!error && response.statusCode === 200){
             const rootChain = JSON.parse(body);
             console.log('replace chain on a sync with', rootChain);
             blockchain.replaceChain(rootChain);
         }
     });
+    request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error,response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootTransactionPoolMap = JSON.parse(body);
+            console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
+        }
+    }); 
 }
 let PEER_PORT;
 if(process.env.GENERATE_PEER_PORT === 'true'){
@@ -41,5 +73,5 @@ if(process.env.GENERATE_PEER_PORT === 'true'){
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
-    syncChains();
+    syncWithRootState();
 });
