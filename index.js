b623ce5d9d7cca6b3e106991e6bbca39894e234c
Uditi <uditiarora@gmail.com>
2020-01-11 14:02:35 +0530

    mining transactions

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const app = express();
 const DEFAULT_PORT = 3000;
+const TransactionMiner = require('./app/transaction-miner');
+
 
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({blockchain, transactionPool,wallet});
+const transactionMiner = new TransactionMiner({blockchain, transactionPool, wallet, pubsub});
 
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
@@ -49,6 +52,12 @@ app.post('/api/transact',(req,res) => {
 app.get('/api/transaction-pool-map',(req,res) => {
     res.json(transactionPool.transactionMap);
 });
+
+app.get('/api/mine-transactions' ,(req,res) => {
+    transactionMiner.mineTransactions();
+    res.redirect('/api/blocks');
+});
+
 const syncWithRootState = () => {
     request({url: `${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body) => {
         if(!error && response.statusCode === 200){
