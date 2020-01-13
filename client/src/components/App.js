823662b1417f07e9b7254a53901fa0cee3ce2e58
Uditi <uditiarora@gmail.com>
2020-01-13 20:54:52 +0530

    making the app run on localhost:3000

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import React,{ Component } from 'react';
+
+
+class App extends Component{
+    state = { 
+        walletInfo: 
+        {address:'fooxv6', balance:9999}
+    };
+    componentDidMount(){
+        fetch('http://localhost:3000/api/wallet-info')
+        .then(response => response.json())
+        .then(json => this.setState({walletInfo: json}));
+    }
+    render() {
+        const {address, balance} = this.state.walletInfo;
+        return (
+            <div>
+                <div>Welcome to blockchain!!</div>
+                <div>Address:{address}</div>
+                <div>Balance:{balance}</div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-    <div>Cryptochain in react: Testing3</div>,
+    <App />,
     document.getElementById('root')
 );
 
