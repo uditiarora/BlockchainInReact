4100184c908d64226753d4657c77cdf05ebb8bcf
Uditi <uditiarora@gmail.com>
2020-01-13 10:19:27 +0530

    making react app work using parcel-bundler

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-    <div>Cryptochain in react</div>
+    <div>Cryptochain in react</div>,
+    document.getElementById('root')
 );
 
-console.log('working');
