3df438b7be81e415d63a3a070af9c2fa7e0b2712
Uditi <uditiarora@gmail.com>
2020-01-13 21:30:25 +0530

    adding seed data

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ app.get('/api/wallet-info',(req,res)=>{
 app.get('*',(req,res) => {
     res.sendFile(path.join(__dirname,'client/dist/index.html'));
 });
+
+
 const syncWithRootState = () => {
     request({url: `${ROOT_NODE_ADDRESS}/api/blocks`},(error,response,body) => {
         if(!error && response.statusCode === 200){
@@ -87,6 +89,46 @@ const syncWithRootState = () => {
         }
     }); 
 }
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({wallet, recipient, amount}) => {
+    const transaction = wallet.createTransaction({
+        recipient, amount, chain: blockchain.chain
+    });
+    transactionPool.setTransaction(transaction);
+
+};
+
+const walletAction = () => generateWalletTransaction({
+    wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+    wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+    wallet: walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+for(let i=0;i<10;i++){
+    if(i%3 === 0) {
+        walletAction();
+        walletFooAction();
+    }
+    else if(i%3 === 1){
+        walletAction();
+        walletBarAction();
+    }
+    else{
+        walletFooAction();
+        walletBarAction();
+    }
+    transactionMiner.mineTransactions();
+}
+
 let PEER_PORT;
 if(process.env.GENERATE_PEER_PORT === 'true'){
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000); 
