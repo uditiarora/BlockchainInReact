a5da173bb4edaeaf2a065b250d1751412eb6d42a
Uditi <uditiarora@gmail.com>
2020-01-14 10:42:53 +0530

    basic frontend done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,35 @@
-import React,{ Component } from 'react';
-import Blocks from './Blocks';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class App extends Component{
-    state = { 
-        walletInfo: 
-        {address:'fooxv6', balance:9999}
-    };
-    componentDidMount(){
-        fetch('http://localhost:3000/api/wallet-info')
-        .then(response => response.json())
-        .then(json => this.setState({walletInfo: json}));
-    }
-    render() {
-        const {address, balance} = this.state.walletInfo;
-        return (
-            <div className= "App">
-                <div>Welcome to blockchain!!</div>
-                <div className="WalletInfo">
-                    <div>Address:{address}</div>
-                    <div>Balance:{balance}</div>
-                </div>
-                
-                <br/>
-                <Blocks />
-            </div>
-        );
-    }
+class App extends Component {
+  state = { walletInfo: {} };
+
+  componentDidMount() {
+    fetch(`${document.location.origin}/api/wallet-info`)
+      .then(response => response.json())
+      .then(json => this.setState({ walletInfo: json }));
+  }
+
+  render() {
+    const { address, balance } = this.state.walletInfo;
+
+    return (
+      <div className='App'>
+        <div>
+          Welcome to the blockchain...
+        </div>
+        <br />
+        <div><Link to='/blocks'>Blocks</Link></div>
+        <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
+        <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
+        <br />
+        <div className='WalletInfo'>
+          <div>Address: {address}</div>
+          <div>Balance: {balance}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
