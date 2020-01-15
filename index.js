2d808683b17f6349a0f17fcd4281bc0aa91ee0e7
Uditi <uditiarora@gmail.com>
2020-01-15 08:23:25 +0530

    adding additional features

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/api/blocks', (req,res) => {
 
 app.get('/api/blocks/length', (req, res) => {
     res.json(blockchain.chain.length);
-  });
+});
   
   app.get('/api/blocks/:id', (req, res) => {
     const { id } = req.params;
@@ -45,7 +45,7 @@ app.get('/api/blocks/length', (req, res) => {
     endIndex = endIndex < length ? endIndex : length;
   
     res.json(blocksReversed.slice(startIndex, endIndex));
-  });
+});
 
 app.post('/api/mine',(req,res) => {
     const {data} = req.body;
@@ -87,6 +87,22 @@ app.get('/api/wallet-info',(req,res)=>{
         balance: Wallet.calculateBalance({chain: blockchain.chain, address})
     });
 });
+
+
+
+app.get('/api/known-addresses', (req, res) => {
+    const addressMap = {};
+  
+    for (let block of blockchain.chain) {
+      for (let transaction of block.data) {
+        const recipient = Object.keys(transaction.outputMap);
+  
+        recipient.forEach(recipient => addressMap[recipient] = recipient);
+      }
+    }
+  
+    res.json(Object.keys(addressMap));
+  });
 
 app.get('*',(req,res) => {
     res.sendFile(path.join(__dirname,'client/dist/index.html'));
